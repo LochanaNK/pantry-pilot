efb5e81a215e1a51c02ffe1441d87a74cdc7ca5b
LochanaNK <lochanakariyawasam10@gmail.com>
2026-01-17 01:12:54 +0530

    project setup with nativewind

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,16 @@
+import { Image } from 'expo-image';
+import { View,Text} from 'react-native';
+import { Link } from 'expo-router';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>
+        Hello
+      </Text>
+    </View>
+  );
+}
+
+
+
