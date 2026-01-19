f39eee64dea567e2646db27bc219ccb5c4a069f6
LochanaNK <lochanakariyawasam10@gmail.com>
2026-01-20 01:13:54 +0530

    #4 refactor tab layout; update header and tab titles, and improve icon rendering

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,28 +1,47 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router";
+import React from "react";
+import { FontAwesome6, Ionicons } from "@expo/vector-icons";
 
 export default function TabLayout() {
-
   return (
     <Tabs
       screenOptions={{
         headerShown: true,
-        headerTintColor:'#57534e',
-        headerTitleStyle:{
-          fontWeight:"bold",
-          fontSize:24
-        }
-      }}>
+        headerStyle: {
+          backgroundColor: "#ffedd5",
+        },
+        headerTintColor: "#ea580c",
+        headerTitleStyle: {
+          fontWeight: "bold",
+          fontSize: 24,
+        },
+        tabBarStyle: {
+          backgroundColor: "#ffedd5",
+        },
+        tabBarInactiveTintColor: "#57534e",
+        tabBarActiveTintColor: "#ea580c",
+        tabBarLabelStyle: {
+          fontSize: 14,
+          fontWeight: "bold",
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: "Recipes",
+          tabBarIcon:({color,focused})=>(
+            <Ionicons name="fast-food-sharp" size={24} color={focused? "#ea580c" : "#57534e"} />
+          )
         }}
       />
       <Tabs.Screen
         name="pantry"
         options={{
-          title: 'MY Pantry',
+          title: "MY Pantry",
+          tabBarIcon: ({ color, focused }) => (
+            <FontAwesome6 name="clipboard-list" size={24} color={focused? "#ea580c" : "#57534e"} />
+          ),
         }}
       />
     </Tabs>
